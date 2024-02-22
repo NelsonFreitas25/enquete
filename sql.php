9510a0f33f1eaa7e17d3019afd61cf7e6fcb40e9
NelsonFreitas25 <127761444+NelsonFreitas25@users.noreply.github.com>
2024-02-22 15:36:00 -0300

    front-end quase terminado , falta algumas alterações

---
+++ sql.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$database = "crud";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+print_r($_POST);
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "INSERT INTO alunos (senha, Email) VALUES ('$senha', '$email')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
